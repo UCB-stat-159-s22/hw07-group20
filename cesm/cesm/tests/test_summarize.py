adb1ba73005e5e20a177849a23f3b5d4e6d5ec6d
Jacob Jossart <jpjossart@berkeley.edu>
2022-05-12 04:54:55 +0000

    same as previous

--- cesm/cesm/tests/test_summarize.py
+++ cesm/cesm/tests/test_summarize.py
@@ -1,31 +1,69 @@
-import os
-import pytest
+#imports 
 import xarray as xr
+import os
+import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+from cesm import cesm_analyze as ca
 from cesm import cesm_summarize as cs
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import shelve
 
 # change temp dir one folder down for correct relative path testing
 os.chdir('cesm')
 
+
 ##########################################################################
 
 def test_average_and_max():
-    models = ['sp1', 'sp2', 'sp3', 'sp5']
+    with shelve.open('../analysis_data/descriptors') as db:
+        vardict = db['vardict']
+        modeldict = db['modeldict']
 
-	test = cs.average_and_max(USA, 'tas')
-	
-    assert len(dtest) == 5
-    assert list(dtest) == ['hist', 'sp1', 'sp2', 'sp3', 'sp5']
-    assert isinstance(dtest['hist'], xr.Dataset)
+    #Opening each of the Xarray datasets
+    ds = ca.dictorize(
+        func = xr.open_zarr, 
+        modeldict = {k: v['code'] for k, v in modeldict.items()},
+    )
+
+    baseline = (
+        ds['hist']
+        .sel(time=slice('1850', '1980'))
+        .mean('time')
+    )
+
+    ds_diffs = ds.copy()
+    ds_diffs['hist'] = baseline
+    ds_diffs = ca.dictorize(
+        func = 'diff',
+        modeldict = ca.nest_dicts(
+            ds_diffs,
+        ),
+    )
+
+    USA = ca.dictorize(
+        func = 'sel',
+        modeldict = ca.nest_dicts(
+            ds_diffs,
+            {'args': {'time': '2100',
+                     'lat': slice(0, 70),
+                     'lon': slice(225,300)}},
+        ),
+    )
+
+    USA = ca.dictorize(
+        func = 'agg',
+        modeldict = ca.nest_dicts(
+            USA,
+            {'grps': 'time.year', 'aggfunc': np.nanmean, 'roll': {}},
+        ),
+    )
 
 
-def average_and_max(data, param):
-    summary = pd.DataFrame(columns = list(data), index = ["Mean", "Max"])
-    for col in summary.columns:
-        mn = data[col][param].mean(("lat","lon")).values[0]
-        summary.loc["Mean", col] = mn
-        mx = data[col][param].max(("lat","lon")).values[0]
-        summary.loc["Max", col] = mx
-    return summary
-       
+
+    test = cs.average_and_max(USA, 'tas')
+
+    assert len(test.columns) == 4
+    assert isinstance(test, pd.DataFrame)
+
